f8b11c7a317ddadf8785fc71901d0aa27d5b94e0
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-10 19:20:54 -0300

    Alterações no Cabeçalho e no Título do programa

--- AreaRetangulo.c
+++ AreaRetangulo.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <locale.h>
 
+/* autor: Gabriel Jorge
+	problema: Desenvolver um programa em C que calcule a area de um retâgulo com a
+	base e a altura fornecidos pelo usuário */
+
 int main(int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese");
   float base, altura, area;
   
+  printf("========== Cálculo de área do Triângulo =============")
   printf("Digite o valor da base: ");
   scanf("%f", &base);
   printf("Digite o valor da altura: ");
