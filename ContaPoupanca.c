ef6ff91c6b842dc195f41e980cf80ed19721f755
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-10 19:42:56 -0300

    Alterações no Cabeçalho e Título do programa

--- ContaPoupanca.c
+++ ContaPoupanca.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <locale.h>
 
+/* autor: Gabriel Jorge
+	problema: Crie um algoritmo que controle uma conta poupança que foi aberta
+	 com um depósito de R$500,00. Sendo a remuneração de 1% ao mês de juros. A presente o saldo após três meses*/ 
+
 int main(int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese");
   
   float deposito;
   deposito = 500;
+  printf("========= Cálculo de rendimento mensal de depósito: ========= \n");
   printf("Primeiro mês R$ %.2f \n",deposito);
   deposito = (0.01 * deposito) + deposito;
   printf("Segundo mês R$ %.2f \n",deposito);
