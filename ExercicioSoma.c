d42785e1c105eae886cb7e8cfd52399a1dd4ac74
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-10 20:20:27 -0300

    Alterações no Cabeçalho e no Título do programa

--- ExercicioSoma.c
+++ ExercicioSoma.c
@@ -2,18 +2,22 @@
 #include <stdlib.h>
 #include <locale.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ /* autor: Gabriel Jorge
+	problema: Crie um algoritmo que some dois números apresentados pelo usuário.
+*/ 
+
 
 int main() {
 	setlocale(LC_ALL, "Portuguese"); 
 
+	printf("============ Soma ============\n");
 	float n1, n2, soma;
-	printf("Digite um número: ");
+	printf("Digite um número: \n");
 	scanf("%f",&n1);
-	printf("Digite outro número: ");
+	printf("Digite outro número: \n");
 	scanf("%f",&n2);
 	soma = n1 + n2;
-	printf("A soma dos números é %6f \n",soma);
+	printf("A soma dos números é %f \n",soma);
 	system("PAUSE");
 	return 0;
 }
