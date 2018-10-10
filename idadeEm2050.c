1c3026cc39444680f17a93857c33fe26f66c8b6c
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-10 20:26:19 -0300

    Alterações no Cabeçalho, estrutura de decisão e Título do programa

--- idadeEm2050.c
+++ idadeEm2050.c
@@ -2,19 +2,29 @@
 #include <stdlib.h>
 #include <locale.h>
 
+ /* autor: Gabriel Jorge
+	problema: Crie um algoritmo que receba o ano de  nascimento de uma pessoa. Calcule e mostre a idade atual e quantos anos essa pessoa terá em 2050.
+
+*/ 
+
 int main(int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese");
   int anonasc, idade, idadefut, anoatual;
-  
+  printf("========= Idade em 2050 ========\n");
   printf("Digite seu ano de nascimento: \n");
   scanf("%d",&anonasc);
   printf("Digite o ano atual \n");
   scanf("%d", &anoatual);
   idade = anoatual - anonasc;
   idadefut = 2050 - anonasc;
+  if (anonasc == 2018) {
+  	printf("Você tá mentindo mentiroso \n");
+  }
+  else {
   printf("Sua idade é: %d \n",idade);
   printf("Em 2050 você terá %d anos\n",idadefut);
+    }
   system("PAUSE");	 
   return 0;
 }
