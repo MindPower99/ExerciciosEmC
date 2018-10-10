e65b189ff92358b5e3d8d28f18754636e2e5d649
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-10 19:32:39 -0300

    Alterações no Cabeçalho, Título do programe e estrutura de decisão

--- BemVindoSenhor(a).c
+++ BemVindoSenhor(a).c
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 #include <locale.h>
 
+/* autor: Gabriel Jorge
+	problema: Desenvolver um programa em C que receba
+	 o sexo da pessoa e printe Bem-vindo caso for homem
+	  e Bem-venda caso for mulher*/ 
+	
 int main(int argc, char *argv[])
 {
   setlocale(LC_ALL, "Portuguese");
   
   char sexo;
+  printf("========== Bem-Vindos ============\n");
   printf("Digite seu sexo \n M - Masculino \n F - Feminino \n ");
   scanf("%c",&sexo);
   if (sexo == 'm') {
      printf("Bem-Vindo Senhor ! \n");         
-  }
-  else
-  {
+	 }
+  else if (sexo == 'f'){
       printf("Bem-Vinda Senhora ! \n ");
-      }
+	}
+	else 
+		printf("Opção inválida. \n");
+      
   system("PAUSE");	
   return 0;
 }
