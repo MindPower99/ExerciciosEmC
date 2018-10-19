514cbf18dc9cc718484849da0f2d6b0ddae2c213
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-18 23:41:44 -0300

    Alterações no sistema de senha

--- ValidarSenha.c
+++ ValidarSenha.c
@@ -8,7 +8,9 @@
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL,"Portuguese");
 	char senha[6];
-	char se[5] = {'a','s','d','f','g'};
+	char se[6];
+	printf("Digite uma nova senha: \n");
+	scanf("%s",&se);
 	printf("Digite a senha : \n");
 	scanf("%s",&senha);
 	if(strcmp(senha,se)==0) {
@@ -17,26 +19,7 @@ int main(int argc, char *argv[]) {
 	else {
 		printf("Senha invalida \n");
 	}
-	return 0;
-}
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <string.h>
- run this program using the console pauser or add your own getch, system("pause") or input loop 
-
-int main(int argc, char *argv[]) {
-	setlocale(LC_ALL, "Portuguese");
-	char senha[6];
-	char se[5] = {'a','s','d','f','g'};
-	printf("Digite a senha: \n");
-	scanf("%s",&senha);
-	if(strcmp(senha,se)==0) {
-		printf("Senha inválida.\n");
-	}
-	else {
-		printf("Senha válida. \n");
-}
 	system("PAUSE");
 	return 0;
-}*/
+	
+}
