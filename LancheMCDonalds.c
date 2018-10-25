f29517f12ad87e7ddcfaefcf52f8769f769a2ecf
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-24 22:02:56 -0300

    Alterações na documentação

--- LancheMCDonalds.c
+++ LancheMCDonalds.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Autor: Gabriel Jorge
+	Problema: Escrever um programa para apresentar o nome do lanche do MC Donalds conforme o nº da opção digitada pelo cliente(usuario):
+	1. Big MC
+	2. Quarteirao
+	3. MC Chicken
+	4. Cheddar MC Melt
+	5. MacMax
+	
+*/
 
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "Portuguese");
 	int opcao;
+	printf("================MC Donald's");
 	printf("Digite o número correspondente ao lanche desejado: \n 1 - BigMac \n 2 - Quarteirão \n 3 - MacChicken \n 4 - Cheddar MacMelt \n 5 - MacMax \n");
 	scanf("%d",&opcao);
 	
