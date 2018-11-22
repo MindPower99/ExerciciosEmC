7f8eddeb4d2fc0c035916c538d31ba49c1df0f41
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-11-22 06:21:20 -0200

    Incrementando o projeto / colocando preços

--- NomeLancheComLaco.c
+++ NomeLancheComLaco.c
@@ -15,31 +15,37 @@ int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "Portuguese");
 	puts("========== Nome dos Lanches do Mc Donalds ==========");
 	int opcao, resp, i;
+	float preco;
 	
 	do {
 		puts("Digite o número do lanche desejado: ");
-			puts("1. BigMac \n 2. Quarteirão \n 3. MacChicken \n 4. Cheddar MacMelt \n 5. MacMax  \n");
+			puts("1. BigMac R$ 10,00  \n 2. Quarteirão R$ 12,00 \n 3. MacChicken R$ 8,00 \n 4. Cheddar MacMelt R$ 10,00 \n 5. MacMax R$ 12,00  \n");
 			scanf("%d",&opcao);
 			
 		switch(opcao) {
 			case 1: 
-				puts("1 - Bic Mac. \n");
+				puts("1. Bic Mac - R$ 10,00 \n");
+				preco += 10;
 				break;
 				
 			case 2: 
-				puts("2 - Quarteirão. \n");
+				puts("2 - Quarteirão - R$ 12,00 \n");
+				preco += 12;
 				break;
 				
 			case 3: 
-				puts("3 - MacChicken. \n");
+				puts("3 - MacChicken - R$ 8,00\n");
+				preco += 8;
 				break;
 			
 			case 4: 
-				puts("4 - Cheddar MacMelt. \n");
+				puts("4 - Cheddar MacMelt - R$ 10,00 \n");
+				preco += 10;
 				break;
 				
 			case 5: 
-				puts("5 - MacMax. \n");
+				puts("5 - MacMax - R$ 12,00 \n");
+				preco += 12;
 				break;
 				
 			default: 
@@ -54,6 +60,7 @@ int main(int argc, char *argv[]) {
 		}
 		
 		else {
+			printf("O valor do pedido é de R$ %2.f \n ",preco);
 			i++;
 		}
 	} while (i < 1);
