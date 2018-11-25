943df52e767e3808254507304e70387c9d5beaff
MindPower99 <gabriel-jorge1999@gmail.com>
2018-11-25 21:56:42 -0200

    Adicionando funcionalidade de ver quem como está o andamento da votação

--- UrnaEletronica.c
+++ UrnaEletronica.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
 	int opcao,voto,bolsonaro, haddad, Ciro, Amoedo, Marina, Alckimin,Alvaro, Boulos, Daciolo,i1 = 0,i2 = 0,certeza,limiterros = 0;
 	char SenhaMesario[] = "senhamesario";
 	char SenhaChefeDeUrna[] = "senhachefedeurna";
+	char SenhaC[20];
 
 	do {
 		puts("Digite o número correspondente a seu candidato: \n 1 - Bolsonaro \n 2 - Haddad \n 3 - Ciro \n 4 - Amoedo \n 5 - Marina \n 6 - Alckimin \n 7 - Álvaro Dias \n 8 - Boulos \n 9 - Daciolo \n");
@@ -93,6 +94,36 @@ int main(int argc, char *argv[]) {
                       }
                       
                       break;
+                      
+                 case 11: 
+                      
+                      puts("Digite a senha de Chefe de Urna.. \n");
+                      scanf("%s",&SenhaC);
+                      
+                      if (strcmp(SenhaC,SenhaChefeDeUrna)==0) {
+                         puts("============ Ranking Votação ==================\n");
+                         
+                         if (bolsonaro > haddad && bolsonaro > Ciro && bolsonaro > Amoedo && bolsonaro > Marina && bolsonaro > Alckimin && bolsonaro > Alvaro && bolsonaro > Boulos && bolsonaro > Daciolo) {
+  				            puts("Bolsonaro é o vencedor ! \n");
+  				            printf("Jair Bolsonaro = %d /n",bolsonaro);
+       				        printf("Fernando Haddad = %d /n",haddad);
+       				        printf("Ciro Gomes = %d /n",Ciro);
+       				        printf("João Amoedo = %d /n",Amoedo);
+       				        printf("Marina Silva = %d /n",Marina);
+       				        printf("Geraldo Alckimin = %d /n",Alckimin);
+       				        printf("Álvaro Dias = %d /n",Alvaro);
+       				        printf("Guilherme Boulos = %d /n",Boulos);
+       				        printf("Cabo Daciolo = %d /n",Daciolo);
+            			 }
+            		  	 else if (haddad > bolsonaro && haddad > Ciro && haddad > Amoedo) {
+       				          puts("Haddad é o vencedor ! \n");
+            			 }
+                                                                 
+                      }
+                      
+                      else {
+                           puts("Senha incorreta.\n");     
+                      }
                                    	
     			default: 
     				puts("Digite uma opção válida \n"); 
