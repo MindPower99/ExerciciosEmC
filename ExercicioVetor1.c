0858e562ea449c6a6ef60c5c8abcba3ab2b74dac
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-11-29 09:49:18 -0200

    Lê 10 elementos de um vetor e apresenta os valores lidos

---
+++ ExercicioVetor1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/* 	
+	Autor: Gabriel jorge
+	Problema: 1. Ler 10 elementos de um vetor,
+	após finalizar a leitura apresentar todos os valores lidos.
+*/
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "Portuguese");
+	int i,i2,n[10];
+	for (i = 0; i < 10; i++) {
+		printf("Digite o %d numero: \n",i+1);
+		scanf("%d",&n[i]);
+	}
+	i = 0;
+	
+	for (i2 = 0; i2 < 10; i2++) {
+		printf("Número %d: %d \n",i+=1,n[i2]);
+	}	
+	return 0;
+}
